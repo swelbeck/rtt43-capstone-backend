5ac680b9a8582d1c5a4c0196e4aeb4ed66942dc4
SWelbeck <swelbeck12@gmail.com>
2024-11-19 12:27:44 -0500

    setting up initial server

---
+++ server.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+import connectDB from "./config/db.mjs";
+import dotenv from "dotenv";
+// import userRoutes from "./routes/api/users.mjs";
+// import authRoutes from "./routes/api/auth.mjs";
+import cors from "cors";
+
+dotenv.config();
+
+//Initialize our app variable with Express
+const app = express();
+
+//Connect Database
+connectDB();
+
+// Initialize middleware
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+//Single endpoint just to test API. Send data to browser
+app.get("/", (req, res) => res.send("API Running"));
+
+//Define Routes
+// app.use("/api/users", userRoutes);
+// app.use("/api/auth", authRoutes);
+
+// Environmental Variables
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
